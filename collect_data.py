8d1c94a5a781aab33298a34235b5536560b54b8b
Lindsey Eggleston <lindsey.eggleston@gmail.com>
2017-06-01 13:03:51 -0600

    Update collect_data.py

--- collect_data.py
+++ collect_data.py
@@ -1,19 +1,45 @@
 import pandas as pd
 import last_eleven_lines
 import parker_get_data
-import clean1
+import clean_first
 from sklearn.preprocessing import StandardScaler
 
 '''
-This script returns a dataframe X of features chosen by each team member as the most important from their assigned subset of the original data, and a dataframe y of labels (fraud or not).
+This script returns a dataframe X of features chosen by each team member as the most important 
+from their assigned subset of the original data, and a dataframe y of labels (fraud or not).
 '''
 
-def get_data(filename='data/data.json', return_y = False):
+def get_data(filename='data/data.json'):
+    '''
+    Retrieves data from json file and converts it into a pandas dataframe.
+    Inputs:
+    -------
+    filename: the json file name/path
+    
+    Output:
+    -------
+    Pandas DataFrame
+    '''
     df = pd.read_json(filename)
-    y = df.acct_type.apply(lambda x: 1 if 'fraud' in x else 0)
-    ben_data = last_eleven_lines.return_top_three(last_eleven_lines.get_data(filename)[-1])
+    return df
+
+def clean_data(df, return_y=False):
+    '''
+    Cleans dataframe
+    Input:
+    ------
+    df: pandas DataFrame
+    return_y: bool, if True it creates a target array from 'acct_type' column
+    
+    Output:
+    -------
+    Cleaned pandas DataFrame, and if return_y = True it creates a target array from 'acct_type' column
+    '''
+    if 'acct_type' in df.columns:
+        y = df.acct_type.apply(lambda x: 1 if 'fraud' in x else 0)
+    ben_data = last_eleven_lines.return_top_three(last_eleven_lines.get_data(df)[-1])
     parker_data = parker_get_data.get_data(df)
-    lindsey_data = clean1.return_import_features(df)
+    lindsey_data = clean_first.return_import_features(df)
     tim_data = df[['num_order', 'num_payouts']]
     X = pd.concat([ben_data, parker_data, tim_data, lindsey_data], axis=1)
     X = pd.get_dummies(X, columns=['user_type'])
@@ -22,10 +48,25 @@ def get_data(filename='data/data.json', return_y = False):
        'sale_duration2', 'sale_duration', 'num_order', 'num_payouts',
        'created-to-start', 'body_length']
     X[numeric_vars] = scaler.fit_transform(X[numeric_vars].values)
-    if return_y:
+    if return_y==True:
         return X, y
-    else:
-        return X
+    return X
+
+def get_column_names(df):
+    '''
+    Retrieves the column names from a pandas DataFrame
+    Input:
+    -------
+    df: a pandas DataFrame
+    
+    Output:
+    -------
+    an array of column names
+    '''
+    return df.columns
 
 if __name__ == '__main__':
-    X, y = get_data(return_y = True)
+    df = get_data()
+    X,y = clean_data(df, return_y=True)
+    cols = get_column_names(X)
+    print(X.head())
