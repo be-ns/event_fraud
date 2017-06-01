da9be622120cf6481a5cc77ceafd721bdf1a5233
Lindsey Eggleston <lindsey.eggleston@gmail.com>
2017-06-01 15:57:08 -0600

    upload everything

--- collect_data.py
+++ collect_data.py
@@ -3,11 +3,20 @@ import last_eleven_lines
 import parker_get_data
 import clean_1
 from sklearn.preprocessing import StandardScaler
+from pymongo import MongoClient
+import requests
+import json
+import pickle
+from gradient_boosting import predict_new_proba
+from collect_data import clean_data
+from datetime import datetime
 
 '''
 This script returns a dataframe X of features chosen by each team member as the most important
 from their assigned subset of the original data, and a dataframe y of labels (fraud or not).
 '''
+with open('data/scaler.pkl','rb') as s:
+    scaler = pickle.load(s)
 
 def get_data(filename='data/data.json'):
     '''
@@ -46,11 +55,47 @@ def clean_data(df, return_y=False):
     scaler = StandardScaler()
     numeric_vars = ['tickets_left', 'types_tickets', 'num_previous_payouts',
        'sale_duration2', 'sale_duration', 'num_order', 'num_payouts',
-       'created-to-start', 'body_length']
+       'created-to-start', 'body_length','venue_name', 'venue_country',
+       'venue_state','user_created']
     X[numeric_vars] = scaler.fit_transform(X[numeric_vars].values)
     if return_y==True:
         return X, y
     return X
+
+def clean_new_data(dic):
+    '''
+    Reads in a dictionary and converts it into a cleaned pandas DataFrame
+    Input:
+    ------
+    dic: a dictionary whos keys equal columns from original data
+
+    Output:
+    -------
+    a pandas DataFrame
+    '''
+    new_dict = {}
+    keep_cols = ['body_length','event_created','event_start',
+        'num_order','num_payouts',
+        'sale_duration2', 'sale_duration']
+    for key in dic.keys():
+        if key in keep_cols:
+            new_dict[key] = dic[key]
+    ticket_lst = dic['ticket_types']
+    new_dict['tickets_left'] = 0
+    for dct in ticket_lst:
+        new_dict['tickets_left'] += (dct['quantity_total'] - dct['quantity_sold'])
+    new_dict['types_tickets'] = len(ticket_lst)
+    new_dict['event_created'] = int(datetime.fromtimestamp(dic['event_created']).strftime('%j'))
+    new_dict['event_start'] = int(datetime.fromtimestamp(dic['event_start']).strftime('%j'))
+    if new_dict['event_created']>new_dict['event_start']:
+        new_dict['created-to-start'] = 365-new_dict['event_created']+new_dict['event_start']
+    else:
+        new_dict['created-to-start'] = new_dict['event_created']-new_dict['event_start']
+    user_type = str(dic['user_type'])
+    new_user_type = 'user_type_'+user_type
+    new_dict[new_user_type]=1
+    return pd.DataFrame(new_dict, columns=new_dict.keys(), index=[0])
+
 
 def get_column_names(df):
     '''
@@ -66,7 +111,28 @@ def get_column_names(df):
     return df.columns
 
 if __name__ == '__main__':
-    df = get_data()
-    X,y = clean_data(df, return_y=True)
-    cols = get_column_names(X)
-    print(X.head())
+    # in keys:      ['body_length', 'num_order', 'num_payouts', 'sale_duration', 'sale_duration2']
+    # not in keys:  ['approx_payout_date', 'channels', 'country', 'currency', 'delivery_method',
+            # 'description', 'email_domain', 'event_created', 'event_end', 'event_published', 'event_start',
+            # 'fb_published', 'gts', 'has_analytics', 'has_header', 'has_logo', 'listed', 'name',
+            # 'name_length', 'object_id', 'org_desc', 'org_facebook', 'org_name', 'org_twitter',
+            # 'payee_name', 'payout_type', 'previous_payouts', 'show_map', 'ticket_types', 'user_age',
+            # 'user_created', 'user_type', 'venue_address',   'venue_country', 'venue_latitude',
+            # 'venue_longitude', 'venue_name', 'venue_state']
+
+    x = requests.get('http://galvanize-case-study-on-fraud.herokuapp.com/data_point').content
+    x_json = json.loads(x)
+    # client = MongoClient()
+    # db = client.pymongo_test
+    # post_to_mongo(x_json, db)
+
+    cols = ['tickets_left', 'types_tickets', 'num_previous_payouts',
+       'sale_duration2', 'sale_duration', 'num_order', 'num_payouts',
+       'created-to-start', 'body_length', 'user_type_1', 'user_type_2',
+       'user_type_3', 'user_type_4', 'user_type_5', 'user_type_103']
+    # df = get_data()
+    # X,y = clean_data(df, return_y=True)
+    # cols = get_column_names(X)
+    # print(X.head())
+
+    print(clean_new_data(x_json))
